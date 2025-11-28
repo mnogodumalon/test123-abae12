977113e5d92d35bee6b16409a29a5694f0621a0c
mnogodumalon <92773221+mnogodumalon@users.noreply.github.com>
2025-11-28 14:49:24 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/test123-abae12/'
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
